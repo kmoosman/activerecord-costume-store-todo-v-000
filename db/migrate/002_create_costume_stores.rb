6c3c99f9ed14bf4a3ac023da25dddc69ced84946
Katie Moosman <moosman.katie@gmail.com>
2019-02-20 02:47:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,6 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
+      t.datetime :opening_date
+      t.datetie :closing_date
       t.timestamps
     end
   end
