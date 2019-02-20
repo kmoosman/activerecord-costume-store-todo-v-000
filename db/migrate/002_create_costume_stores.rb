fa70ca6c67458607632293851234cbbc4a7c6d30
Katie Moosman <moosman.katie@gmail.com>
2019-02-20 02:13:39 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,12 +10,3 @@ class CostumeStore < ActiveRecord::Migration[4.2]
     end
   end
 end
-
-
-name
-location
-number of costumes, or "costume inventory"
-number of employees
-whether or not it's still in business
-opening time
-closing time
